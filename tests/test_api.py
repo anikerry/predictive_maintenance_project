18e0a70617d75f1d94922cf39c918b2a95994112
Aniket Kulkarni <anikerry@gmail.com>
2026-03-25 08:56:41 +0100

    Add CI workflow for ML pipeline and implement API unit tests

---
+++ tests/test_api.py
@@ -0,0 +1,38 @@
+from fastapi.testclient import TestClient
+from src.api import app  # Imports your FastAPI app
+
+# Initialize the test client
+client = TestClient(app)
+
+def test_health_check():
+    """Test if the API boots up and the root endpoint works."""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "Predictive Maintenance API is running" in response.json()["message"]
+
+def test_predict_healthy_machine():
+    """Test if the model correctly predicts a healthy machine."""
+    # This is the exact dummy data we used in Swagger UI
+    payload = {
+        "Air_temperature_K": 298.1,
+        "Process_temperature_K": 308.6,
+        "Rotational_speed_rpm": 1551.0,
+        "Torque_Nm": 42.8,
+        "Tool_wear_min": 0.0,
+        "Type_L": 0,
+        "Type_M": 1
+    }
+    
+    response = client.post("/predict", json=payload)
+    
+    # 1. Check if the request was successful
+    assert response.status_code == 200
+    
+    data = response.json()
+    
+    # 2. Check if the model predicted "Healthy"
+    assert data["machine_status"] == "Healthy ✅"
+    
+    # 3. Check if the probability is formatted correctly
+    assert "%" in data["failure_probability"]
+    assert data["sensor_data_processed"] is True
